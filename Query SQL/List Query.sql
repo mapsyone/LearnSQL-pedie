b5e4553ab5aaadee11bd2f5d6e479f9256e8742a
unknown <ahmfrdns@gmail.com>
2025-07-27 13:23:13 +0700

    update

--- Query SQL/List Query.sql
+++ Query SQL/List Query.sql
@@ -32,8 +32,8 @@ WHERE
 date(date) BETWEEN '2020-01-01'and '2020-12-31' 
 
 
-GROUP BY 1,2
-ORDER BY 1,3,2
+GROUP BY 1,2     --- group it means like a column
+ORDER BY 1,3,2   --- order it's like A to Z column, if you want use query order you have to make group or colum first.
 
 
 --- Next query
